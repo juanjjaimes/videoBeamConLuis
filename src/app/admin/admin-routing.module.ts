cd8ebb9ccc95f90baea5f387f6dfcf2369173fdb
Juan Jaimes <juanjj213@gmail.com>
2022-04-13 10:57:41 -0500

    navAdmin

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdministrarUsersComponent } from './components/administrar-users/administrar-users.component';
 import { AdministrarVideoBeamsComponent } from './components/administrar-video-beams/administrar-video-beams.component';
+import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'gestionar-user', pathMatch: 'full' },
+  // { path: '', redirectTo: 'gestionar-user', pathMatch: 'full' },
+  { path: '', component: NavComponent },
   { path: 'gestionar-user', component: AdministrarUsersComponent },
   { path: 'gestionar-vb', component: AdministrarVideoBeamsComponent },
 ];
